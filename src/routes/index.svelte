be61974c4761ed0836547dd2c6564a6cf8823320
Spase Markovski <smarkovski@gmail.com>
2021-06-23 00:05:57 +0200

    update to latest svelte-kit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,5 @@
 <main>
   <h1>Hello world!</h1>
 
-  <Counter />
-
-  <p>Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+  <p>My name is, my name is shshsksks</p>
 </main>
